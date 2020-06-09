a9aa0789de033d5ca26030613e0ffec81c90aa60
PskCluB <passakon_p@hotmail.com>
2020-06-09 08:19:29 +0700

    Fix form

--- form-payment-step.js
+++ form-payment-step.js
@@ -1,12 +1,31 @@
 Vue.component('payment-step', {
+  data: function () {
+    return {
+      card_number: '',
+      card_name: '',
+      exp: '',
+      cvv: ''
+    }
+  },
   template: `<div>
   <form-step-bar :step="2"></form-step-bar>
-<h1>payment-step</h1>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="border p-5">
+        <text-input-optional name="หมายเลขบัตร" placeholder="0000 0000 0000 0000" rules="required"
+                             v-model="card_number"/>
+        <text-input-optional name="ชื่อที่ปรากฎบนบัตร" placeholder="กรอกชื่อบนบัตรเครดิต/เดบิต" rules="required"
+                             v-model="card_name"/>
+        <text-input-optional name="วันหมดอายุ" placeholder="MM/YY" rules="required" v-model="exp"/>
+        <text-input-optional name="CVV" placeholder="000" rules="required" v-model="cvv"/>
+      </div>
+    </div>
+  </div>
 
-<div class="row justify-content-center">
-          <div class="d-flex justify-content-center my-4">
-            <button class="btn btn-blue btn-block" type="button" @click="$emit('changeStep',3)">สมัคร</button>
-          </div>
-        </div>
+  <div class="row justify-content-center">
+    <div class="d-flex justify-content-center my-4">
+      <button class="btn btn-blue btn-block" type="button" @click="$emit('changeStep',3)">ชำระเงิน</button>
+    </div>
+  </div>
 </div>`
 })
